92147e1ef29df578b846fb32e0b6620116c47edc
Onur Alp AKIN <onralpakin@gmail.com>
2023-07-08 12:11:38 +0300

    delete wrapper

--- safeeyes-run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ -f "${XDG_CONFIG_HOME}/safeeyes/safeeyes.json" ]]; then
-    safeeyes "$@"
-else
-    echo "Created safeeyes.json"
-    mkdir -p "${XDG_CONFIG_HOME}/safeeyes/style" && touch "${XDG_CONFIG_HOME}/safeeyes/safeeyes.json"
-    safeeyes "$@"
-fi
